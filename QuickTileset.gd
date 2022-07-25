3e30a990372f173b56afcb20e5edfc0a8eb38a8c
paradrogue <78031990+paradrogue@users.noreply.github.com>
2022-07-25 03:58:46 +0100

    Initial commit

---
+++ QuickTileset.gd
@@ -0,0 +1,181 @@
+tool
+extends TileMap
+
+### ---------------------------------------------------------------------------
+### # QuickTileset (for Godot)
+###
+### A quick and dirty script for creating complete Godot TileSets without hours
+### of endless clicking.
+###
+### Version: 0.1.0
+### Author: <paradrogue@gmail.com>
+### URL: 
+###
+### See LICENSE.md for licensing.
+### ---------------------------------------------------------------------------
+
+# A notional maximum number of tiles to create.
+# More than this will generate a warning.
+const MAX_TILES_LIMIT := 1024
+
+# Common tile sizes for trying to guess cell size.
+const COMMON_TILE_SIZES := [8, 12, 16, 18, 24, 32, 48, 64, 72, 96, 128, 192, 256]
+
+export (Texture) var _texture:Texture setget _set_texture
+export (Vector2) var _offset := Vector2.ZERO setget _set_offset
+export (Vector2) var _padding := Vector2.ZERO setget _set_padding
+export (bool) var _ignore_tile_limits := false setget _set_ignore_tile_limits
+export (int) var _custom_tile_limit := MAX_TILES_LIMIT
+export (bool) var _fix_tilemap_on_texture_change := false
+export (bool) var _guess_tile_size := true
+
+onready var _previous_cell_size := cell_size
+var _clear_existing_tileset := false
+
+func _init() -> void:
+	# Only perform this method when running in the Godot editor.
+	if not Engine.is_editor_hint():
+		return
+
+	# Connect signals
+	_connect_setting_signal()
+#	_connect_signals()
+
+	if tile_set:
+		push_warning(tr("QuickTileset: A TileSet already exists in this TileMap. This will be overridden if any changes are made."))
+
+func _refresh_tileset() -> void:
+	# Only perform this method when running in the Godot editor.
+	if not Engine.is_editor_hint():
+		return
+
+	# Make sure a tileset texture has been supplied.
+	if not _texture:
+		push_warning(tr("QuickTileset: No tiled texture is supplied"))
+		return
+
+	print(tr("QuickTileset: Refreshing TileSet"))
+
+	# If no tileset exists, create one.
+	if tile_set == null:
+		tile_set = TileSet.new()
+		property_list_changed_notify()
+
+	# Try and work out how many cells there are vertically
+	# and horizontally according to the cell size.
+	var cells_h_guess := _texture.get_width() / (cell_size.x + _offset.x + _padding.x)
+	var cells_v_guess := _texture.get_height() / (cell_size.y + _offset.y + _padding.y)
+
+	# Round this down.
+	var cells_h := int(cells_h_guess)
+	var cells_v := int(cells_v_guess)
+
+	# If they don't match exactly, warn the user.
+	if cells_h != cells_h_guess or cells_v != cells_v_guess:
+		push_warning(tr("QuickTileset: Tile texture is not an exact multiple of %s pixels (%s offset, %s padding)-it may not work as expected." % [cell_size, _offset, _padding]))
+
+	var total_tiles := cells_h * cells_v
+	if total_tiles > _custom_tile_limit:
+		push_warning(tr("QuickTileset: A large number (%d) of tiles will be created.  This may cause performance issues." % total_tiles))
+		if _ignore_tile_limits:
+			push_warning(tr("QuickTileset: The tile limit (%d) is being ignored." % _custom_tile_limit))
+		else:
+			push_warning(tr("QuickTileset: The tile limit (%d) has prevented the TileSet from being generated. Aborting." % _custom_tile_limit))
+			return
+
+	# Clear the entire tileset if there is a need to.
+	if _clear_existing_tileset:
+		# Clear the tileset.
+		tile_set.clear()
+		# Remove any other tiles.  The above doesn't always work
+		# as expected...
+		for id in tile_set.get_tiles_ids():
+			tile_set.remove_tile(id)
+
+	# Create the tiles.
+	for y in cells_v:
+		for x in cells_h:
+			# Work of the index of the tile.
+			var index = (y * cells_h) + x
+			# Create a new tile.
+			tile_set.create_tile(index)
+			# Set the texture.
+			tile_set.tile_set_texture(index, _texture)
+			# Calculate the Rect2 representing the region
+			# of the texture.
+			var rect := Rect2(_offset+Vector2(x*(cell_size.x+_padding.x),y*(cell_size.y+_padding.y)), cell_size)
+			# Set the tile's region.
+			tile_set.tile_set_region(index, rect)
+	
+	property_list_changed_notify()
+
+# Setget handler for _texture
+func _set_texture(value:Texture) -> void:
+	_texture = value
+	_clear_existing_tileset = true
+	_refresh_tileset()
+
+	# Fix invalid tiles, if allowed.
+	if _fix_tilemap_on_texture_change:
+		fix_invalid_tiles()
+
+	# Guess tile size, if allowed.
+	if _guess_tile_size:
+		_guess_tile_size_from_texture()
+
+
+# Setget handler for _offset
+func _set_offset(value:Vector2) -> void:
+	_offset = value
+	_clear_existing_tileset = true
+	_refresh_tileset()
+
+# Setget handler for _padding
+func _set_padding(value:Vector2) -> void:
+	_padding = value
+	_clear_existing_tileset = true
+	_refresh_tileset()
+
+# Setget handler for _ignore_tile_limits
+func _set_ignore_tile_limits(value:bool) -> void:
+	_ignore_tile_limits = value
+	_clear_existing_tileset = true
+	_refresh_tileset()
+
+# Listener for when the TileMap's settings have changed.
+# It compares the current cell size with the previous cell size.
+# If it has changed, refresh the tileset.
+func _on_settings_changed() -> void:
+	if _previous_cell_size != cell_size:
+		_previous_cell_size = cell_size
+		_refresh_tileset()
+
+# Connect the settings_changed signal. This updates
+# the TileSet only if the cell size is changed.
+func _connect_setting_signal() -> void:
+	if not is_connected("settings_changed", self, "_on_settings_changed"):
+		var _discard := connect("settings_changed", self, "_on_settings_changed")
+
+# Attempt to discent the
+func _guess_tile_size_from_texture() -> void:
+	# Get the list of sizes
+	var sizes := COMMON_TILE_SIZES
+	# Invert it so we deal with the longest strings first
+	# (less likely to be discovered accidentally).
+	sizes.invert()
+
+	# Get the texture's file name (well, path, but it will do).
+	var texture_file_name := _texture.resource_path
+
+	# Check each of the size combinations and see if we can find one.
+	for w in sizes:
+		for h in sizes:
+			# If we find one, assign it to cell_size and update
+			# the editor.
+			if texture_file_name.count("%dx%d" % [w, h]):
+				self.set_cell_size(Vector2(w,h))
+				push_warning(tr("QuickTileset: Texture assumed to contain tiles of size %s.  Update cell_size manually if incorrect." % str(Vector2(w,h))))
+				property_list_changed_notify()
+				return
+
+	push_warning(tr("QuickTileset: Could not determine cell size.  Update cell_size manually."))
---
+++ README.md
@@ -0,0 +1,53 @@
+# QuickTileset (for Godot)
+
+A quick and dirty script for creating complete Godot `TileSet`s without the hours of endless clicking.
+
+Version: 0.1.0
+
+License: MIT
+
+## Background
+
+I needed a quick way of trying out various pre-made tile sets, but constructing them by hand in the Godot editor was tedious and error prone.
+
+This script takes a texture containing a tile set, configure the cell size (and optional offset and per-cell padding) and it will quickly (and automatically) create a full `TileSet` of all tiles in the texture, which is then added to the `TileMap` node.
+
+## Features
+
+- Automatically creates a `TileSet` containing single tiles for each tile in the texture when the texture is added.
+- Automatically updates the `TileSet` if the texture, offset, padding and/or cell size changes.
+- The script can be detached after the `TileSet` has been configured.
+- The script can be re-added non-destructively, as long as no changes are made.
+- If the script it detached, the `TileSet` can be treated like a normal `TileSet` resource.
+- The script attempts to automatically discern the cell size iff the filename contains a "common" tile size in the form `<width>x<height>`, *e.g.* `grass_16x16.png` will be interpreted to contain 16×16 tiles.  Sizes assumed "common" are: 8, 12, 16, 18, 24, 32, 48, 64, 72, 96, 128, 192, 256.  Dimensions are not required to be square, *i.e.* `village_128x64.png` would also be automatically discovered with a cell size of 128×64 pixels. If the cell size is incorrectly determined from the texture's filename, the `cell size` can be corrected manually.
+
+## Known limitations
+
+- Generates an embedded `TileSet` resource only.  Cannot generate an external `TileSet` resource, although the generated resource can be saved to the project as normal.
+- Generates single tiles only.  Cannot generate tiles that span multiple cells.
+- Tiles will be created for each cell in the texture, even if it is not expected or designed to be used.
+- If the `TileSet` has been customised after the script has been detached, it *may* no longer contain the customisations if regenerated, especially if the changes applied were to the tiles themselves.
+- If the script is reattached after it has been detached, it will no longer be aware of the original texture, offset or padding.  The previously generated  `TileSet` will remain until the `TileMap` is updated.
+- A *soft* limit of 1024 tiles is in place.  This is to prevent system lockup as the generator script uses the main Godot thread. This limit can be customised and disabled if required.  Use with caution.
+- If the cell size cannot be determined from the filename, the last-used cell size will remain.  This can be customised.
+- No collisions, occlusion, navigation, *etc.* are generated.
+- Only tested with the following versions of Godot Engine:
+  - 3.4.4.stable (MacOS)
+
+## How to use it
+
+1. Download the script `QuickTileset.gd` and add it to the project.
+2. Create a `Tilemap` node in the scene.
+3. Adjust the `Tilemap`'s Cell Size property to fit the cell size of the tiles in the texture.
+4. Attach the `QuickTileset.gd` script to the new `Tilemap` node.
+5. Adjust the `Offset` and `Padding` script variables as required. Negative values are allowed to cater for overlapping tiles.
+6. Drag and drop (or otherwise `load`) the tile set texture into the `Tileset texture` script variable.
+7. Use the generated `TileSet` within the `TileMap` as normal.
+8. Once finished, the script can be removed from the `TileMap` node.  The `TileSet` will remain as an embedded resource of the `TileMap` node.
+
+An optional `QuickTileset.tscn` is also included if you want a simple scene to instance.
+
+## Version history
+
+`0.1.0` - 2022-07-24
+- Initial release.
